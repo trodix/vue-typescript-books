1649aaeaecfc7e04405dec4843b0a3e2cfbaf29e
Sebastien Vallet <sebastien.vallet89@gmail.com>
2019-05-01 13:40:44 +0200

    setup ts rules + sidebar with router

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+
+<v-container>
+    <v-layout>
+    <h1>Dashboard</h1>
+  </v-layout>
+</v-container>
+
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+export default class Dashboard extends Vue {}
+</script>
